bfcb6adf5ffa9ff19d4b9832333d9b480af92fec
Minkyung <mminkyung.k@gmail.com>
2021-12-28 22:23:20 +0900

    Use services

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const authRouter = require('./routes/auth');
-const axios = require('axios');
+const carService = require('../services/car');
+const kakaotalkService = require('../services/kakaotalk');
 
 module.exports = () => {
   const app = Router();
+
+  authRouter(app);
 
   app.get('/', async (req, res, next) => {
     try {
@@ -14,7 +17,13 @@ module.exports = () => {
       //   return res.redirect('/auth');
       // }
 
-      const cars = await getFastDeliveryCars();
+      const cars = await carService.findAll();
+
+      if (token) {
+        const favoriteCars = carService.filter(cars);
+        await kakaotalkService.send({ cars, favoriteCars, token });
+      }
+
       return res.json(cars);
 
     } catch (e) {
